ddc8868c463a5c974cd6b29c8a6707a4d2d0dabf
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-10-23 13:48:05 -0300

    minhas adicoes

--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -11,6 +11,7 @@ export default class Conversor extends Component {
         };
 
         this.handleConvert = this.handleConvert.bind(this);
+        this.handleValor = this.handleValor.bind(this);
     }
     handleConvert(){
         const countries_convert = `${this.props.moedaA}_${this.props.moedaB}`;
@@ -19,18 +20,23 @@ export default class Conversor extends Component {
         fetch(url).then(res => {
             return res.json();
         }).then(json => {
-            // console.log(json[countries_convert]);
             let cotacao = json[countries_convert];
             this.setState({ moedaB_valor: (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2) });
         });
+    }
+    handleValor(event){
+        if(isNaN(event.target.value)){
+            return 0;
+        }
+        return this.setState({ moedaA_valor: event.target.value });
     }
     render(){
         return(
             <div className="conversor">
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-                <input type="text" onChange={ event => this.setState({ moedaA_valor: event.target.value }) } value={this.state.moedaA_valor}></input>
-                <input type="button" value="converter" onClick={this.handleConvert}></input>
-                <h2>{this.state.moedaB_valor}</h2>
+                <input type="text" onChange={this.handleValor} value={this.state.moedaA_valor} />
+                <input type="button" value="converter" onClick={this.handleConvert} />
+                <h2>{isNaN(this.state.moedaB_valor) ? 0 : this.state.moedaB_valor}</h2>
             </div>
         );
     }
